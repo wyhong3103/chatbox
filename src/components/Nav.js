048bbca109a619e66873e09b44ae32f36ede2c8d
wyhong3103 <wyhong3103@gmail.com>
2023-01-06 14:28:27 +0800

    Finish up basic structure of nav comp

---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+export const Nav = () => {
+    return(
+        <nav>
+            <div className="nav-left">
+                <h1 className="title">
+                    ChatBox
+                </h1>
+            </div>
+            <div className="nav-right">
+                <ul>
+                    <li>
+                        Lobby
+                    </li>
+                    <li>
+                        Sign Out
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+    )
+};
