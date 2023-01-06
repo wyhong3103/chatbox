04789e7c0dcd81a37c4f32958ff9e81d771d3330
wyhong3103 <wyhong3103@gmail.com>
2023-01-06 14:03:06 +0800

    Add routes to each pages

--- src/App.js
+++ src/App.js
@@ -1,3 +1,28 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Lobby } from './pages/Lobby';
+import { Login } from './pages/LogIn';
+import { Room } from './pages/Room';
+
 export const App = () => {
-    
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route 
+                    path="/login"
+                    element={<Login/>}
+                />
+                <Route
+                    path="/lobby"
+                    element={<Lobby/>}
+                />
+                <Route
+                    path="/room/:id"
+                    element={<Room/>}
+                />
+                {/*
+                    404 Not Found Page
+                */}
+            </Routes>
+        </BrowserRouter>
+    )
 };
