87f991f1f054e709c7a1e97bc6dd4387e8c73fb4
wyhong3103 <wyhong3103@gmail.com>
2023-01-06 22:22:19 +0800

    Add context provider

--- src/App.js
+++ src/App.js
@@ -3,29 +3,41 @@ import { Lobby } from './pages/Lobby';
 import { Login } from './pages/LogIn';
 import { Room } from './pages/Room';
 import './styles/General.css';
+import { AccountContext } from './context/AccountContext';
+import { useState } from 'react';
 
 export const App = () => {
+    
+    const [isSignedIn, ,setIsSignedIn] = useState(false);
+    const [currentProfile, ,setCurrentProfile] = useState({});
+
     return(
         <div className='main'>
-            <BrowserRouter>
-                <Routes>
-                    <Route 
-                        path="/login"
-                        element={<Login/>}
-                    />
-                    <Route
-                        path="/"
-                        element={<Lobby/>}
-                    />
-                    <Route
-                        path="/room/:id"
-                        element={<Room/>}
-                    />
-                    {/*
-                        404 Not Found Page
-                    */}
-                </Routes>
-            </BrowserRouter>
+            <AccountContext.Provider value ={
+                {
+                    isSignedIn, currentProfile
+                }
+            }>
+                <BrowserRouter>
+                    <Routes>
+                        <Route
+                            path="/login"
+                            element={<Login setSigned={setIsSignedIn} setCurProfile={setCurrentProfile}/>}
+                        />
+                        <Route
+                            path="/"
+                            element={<Lobby setSigned={setIsSignedIn} setCurProfile={setCurrentProfile}/>}
+                        />
+                        <Route
+                            path="/room/:id"
+                            element={<Room setSigned={setIsSignedIn} setCurProfile={setCurrentProfile}/>}
+                        />
+                        {/*
+                            404 Not Found Page
+                        */}
+                    </Routes>
+                </BrowserRouter>
+            </AccountContext.Provider>
         </div>
     )
 };
