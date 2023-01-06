3ff7446fc315290e2b7b1cbdbb5693e19505fad9
wyhong3103 <wyhong3103@gmail.com>
2023-01-06 15:44:12 +0800

    Create login box and chat box title

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,14 +1,19 @@
+import '../styles/Login.css';
+
 export const Login = () => {
     
     const googleIcon = require('../assets/google.png');
 
     return(
         <div className="login-cont">
-            <h3>Please sign in with your google account to continue.</h3>
-            <button>
-                <img src={googleIcon} alt="Google Icon"/>
-                <span>Sign In</span>
-            </button>
+            <h1>Chat Box</h1>
+            <div className="login-box">
+                <h3>Please sign in with your google account to continue.</h3>
+                <button>
+                    <img src={googleIcon} alt="Google Icon"/>
+                    <span>Sign In</span>
+                </button>
+            </div>
         </div>
     )
 };
