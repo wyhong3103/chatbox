6dfeb602a293a7848cbba5c593fe77921bdc0032
wyhong3103 <wyhong3103@gmail.com>
2023-01-06 14:15:01 +0800

    Finish up basic structure of LogIn page

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,3 +1,14 @@
 export const Login = () => {
+    
+    const googleIcon = require('../assets/google.png');
 
+    return(
+        <div className="login-cont">
+            <h3>Please sign in with your google account to continue.</h3>
+            <button>
+                <img src={googleIcon} alt="Google Icon"/>
+                <span>Sign In</span>
+            </button>
+        </div>
+    )
 };
