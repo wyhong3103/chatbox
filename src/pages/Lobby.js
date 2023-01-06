6ede6e52b2dfcc4a01226407ebc85f8be6493336
wyhong3103 <wyhong3103@gmail.com>
2023-01-06 14:28:41 +0800

    Finish up basic structure of lobby page

--- src/pages/Lobby.js
+++ src/pages/Lobby.js
@@ -1,3 +1,34 @@
-export const Lobby = () => {
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { Nav } from '../components/Nav';
 
+export const Lobby = () => {
+    // use Context later
+    const [keyword, setKeyword] = useState("");
+    let isSignedIn = true;
+
+    return(
+        <div>
+            {
+                isSignedIn ?
+                <div>
+                    <Nav/>
+                    <div className='lobby-cont'>
+                        <label htmlFor='keyword-inp'>
+                            Enter a keyword to join a chat box.
+                            <input 
+                                value={keyword} 
+                                onChange={(e) => setKeyword(e.target.value)}
+                                id="keyword-inp"
+                            />
+                        </label>
+                    </div>
+                </div>
+
+                :
+
+                <Navigate to="/login"/>
+            }
+        </div>
+    ) 
 };
