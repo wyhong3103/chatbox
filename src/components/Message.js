4d483b12956ff3e619c5f0157a062919ba4d2e10
wyhong3103 <wyhong3103@gmail.com>
2023-01-06 14:40:19 +0800

    Add self message structure

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,16 +3,31 @@ export const Message = ({msg}) => {
         msg = {
             name,
             profielUrl,
-            content
+            content,
+            isSelf
         }
     */
     return(
-        <div className="msg-box">
-            <img className="msg-pfp" src={msg.profileUrl} alt={`${msg.name} Profile Pic`}/>
-            <p className="msg-name">{msg.name}</p>
-            <div className="msg-content">
-                {msg.content}
-            </div>
+        <div>
+            {
+                msg.isSelf ?
+                
+                <div className="self-msg-box">
+                    <div className="self-msg-content">
+                        {msg.content}
+                    </div>
+                </div>
+
+                :
+
+                <div className="msg-box">
+                    <img className="msg-pfp" src={msg.profileUrl} alt={`${msg.name} Profile Pic`}/>
+                    <p className="msg-name">{msg.name}</p>
+                    <div className="msg-content">
+                        {msg.content}
+                    </div>
+                </div>
+            }
         </div>
     )    
 };
