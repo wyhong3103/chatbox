f1fda88c52535611cebdc90c921fa84e8d381278
wyhong3103 <wyhong3103@gmail.com>
2023-01-06 23:57:33 +0800

    Fix typo

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,8 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from './firebaseConfig';
 import {getAuth} from 'firebase/auth';
-import {getFireStore} from 'firebase/firestore';
+import {getFirestore} from 'firebase/firestore';
 
 const app = initializeApp(firebaseConfig);
 getAuth(app);
-getFireStore(app);
+getFirestore(app);
